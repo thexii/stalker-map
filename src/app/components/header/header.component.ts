0f331725cd0ecb52703af53c2bc94d66306268e7
Oleksii Zelenskyi <oleksii.zelenskyi@evinent.com>
2024-09-11 23:44:41 +0300

    shoc languages

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -12,7 +12,7 @@ import { TranslateModule, TranslateService } from '@ngx-translate/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  public readonly avaliableLanguages: string[] = ["ua", "en", "ru", "pl", "fr", "de", "esp", "it", "cz"];
+  public readonly avaliableLanguages: string[] = ["ua", "en", "ru", "pl", "fr", "de", "esp", "it", "cz", 'hg', 'chn', 'jpn', 'kor'];
   private readonly lastLanguageCacheKeyString: string = "language";
   private readonly defaultLocale: string = "en";
   public selectedLanguage: string = "";
