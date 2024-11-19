c323e69e81d882a8cdcb03c70d63a1f4c8ca93d0
Oleksii Zelenskyi <oleksii.zelenskyi@evinent.com>
2024-11-20 00:31:58 +0200

    updates to prev

--- src/app/services/analytics.service.ts
+++ src/app/services/analytics.service.ts
@@ -7,23 +7,23 @@ import { getAnalytics } from "firebase/analytics";
 })
 export class AnalyticsService {
 
-  private readonly config = {
-    apiKey: "AIzaSyDB32mndR7uGFr_-w5G9-w8o05832TjauI",
-    authDomain: "stalker-map-online.firebaseapp.com",
-    projectId: "stalker-map-online",
-    storageBucket: "stalker-map-online.appspot.com",
-    messagingSenderId: "412308123213",
-    appId: "1:412308123213:web:179813ce72e454c9f3f604",
-    measurementId: "G-W3X4BS4H20"
-  };
-
   constructor()
   {
 
   }
 
   public init(): void {
-    const app = initializeApp(this.config);
+    let config = {
+      apiKey: process.env['apiKey'],
+      authDomain: process.env['authDomain'],
+      projectId: process.env['projectId'],
+      storageBucket: process.env['storageBucket'],
+      messagingSenderId: process.env['messagingSenderId'],
+      appId: process.env['appId'],
+      measurementId: process.env['measurementId']
+    };
+
+    const app = initializeApp(config);
     const analytics = getAnalytics(app);
   }
 
