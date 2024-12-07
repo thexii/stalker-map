ac185273196fbf5ec879b0df483e8e3419c246e9
thexii <olexiy.zelenskiy@gmail.com>
2024-12-07 15:08:42 +0200

    release 07.12

---
+++ src/app/components/map-hoc/artefact-spawner-popup/artefact-spawner-popup.component.ts
@@ -0,0 +1,97 @@
+import { Component, Input } from '@angular/core';
+import { Artefact, ArtefactSpawner, ArtefactSpawnerConfig, ArtefactSpawnerType } from '../../../models/hoc/map-hoc';
+import { NgFor, NgIf } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+import { Item } from '../../../models/item.model';
+import { TooltipDirective } from '../../tooltips/tooltip.directive';
+import { ItemTooltipComponent } from '../../tooltips/item-tooltip/item-tooltip.component';
+
+@Component({
+  selector: 'app-artefact-spawner-popup',
+  standalone: true,
+  imports: [NgIf, NgFor, TranslateModule, TooltipDirective],
+  templateUrl: './artefact-spawner-popup.component.html',
+  styleUrl: './artefact-spawner-popup.component.scss'
+})
+export class ArtefactSpawnerPopupComponent {
+  @Input() public artefactSpawner: ArtefactSpawner;
+  @Input() public artefactSpawnerData: ArtefactSpawnerConfig;
+  @Input() public items: Item[];
+
+  public config: ArtefactSpawnerType;
+  public useListOfArtifacts: boolean;
+  public artefacts: ArtefactItem[][];
+
+  public itemTooltipComponent: any = ItemTooltipComponent;
+
+  public static readonly excludeArchiArtifacts: string = "EArtifactSpawnerExcludeRule::ExcludeArchiArtifacts";
+
+  private async ngOnInit(): Promise<void> {
+    if (this.artefactSpawner.spawner) {
+      let config = this.artefactSpawnerData.configs.find(x => x.name == this.artefactSpawner.spawner);
+      if (config && config.settings && config.settings.length > 0) {
+        this.artefacts = [];
+
+        if (config.useListOfArtifacts) {
+          this.useListOfArtifacts = true;
+          if (config.listOfArtifacts && config.listOfArtifacts.length > 0) {
+            for (let art of config.listOfArtifacts) {
+              let model = new ArtefactItem();
+
+              let artefact = this.artefactSpawnerData.artefacts.find(x => x.name == art);
+              let item = this.items.find(x => x.uniqueName == art);
+
+              if (item && artefact) {
+                model.artefact = artefact;
+                model.item = item;
+                this.artefacts.push([model]);
+              }
+            }
+          }
+        }
+        else {
+          let common = this.artefactSpawnerData.artefacts.filter(x => x.rarity == 'EArtifactRarity::Common');
+          let uncommon = this.artefactSpawnerData.artefacts.filter(x => x.rarity == 'EArtifactRarity::Uncommon');
+          let rare = this.artefactSpawnerData.artefacts.filter(x => x.rarity == 'EArtifactRarity::Rare');
+          let epic = this.artefactSpawnerData.artefacts.filter(x => x.rarity == 'EArtifactRarity::Epic');
+
+          if (config.excludeRules && config.excludeRules.includes(ArtefactSpawnerPopupComponent.excludeArchiArtifacts)) {
+            epic = epic.filter(x => x.archiartifactType == 'EArchiartifactType::None');
+          }
+
+          this.createArtefactItems(common);
+          this.createArtefactItems(uncommon);
+          this.createArtefactItems(rare);
+          this.createArtefactItems(epic);
+        }
+
+        this.config = config;
+      }
+    }
+  }
+
+  private createArtefactItems(artefacts: Artefact[]): void {
+    let result: ArtefactItem[] = [];
+
+    for (let art of artefacts) {
+      let model = new ArtefactItem();
+
+      let item = this.items.find(x => x.uniqueName == art.name);
+
+      if (item) {
+        model.artefact = art;
+        model.item = item;
+        result.push(model);
+      }
+    }
+
+    if (result.length > 0) {
+      this.artefacts.push(result);
+    }
+  }
+}
+
+class ArtefactItem {
+  public artefact: Artefact;
+  public item: Item;
+}
--- src/app/models/hoc/map-hoc.ts
+++ src/app/models/hoc/map-hoc.ts
@@ -6,6 +6,9 @@ export class MapHoc {
 
   public markers: Marker[];
   public stuffs: Stuff[];
+  public anomalyFields: Marker[];
+  public artefactSpawners: ArtefactSpawner[];
+  public artefactSpawnerData: ArtefactSpawnerConfig;
 }
 
 export class ObjectAtLocationHoc {
@@ -26,3 +29,41 @@ export class Stuff extends ObjectAtLocationHoc {
   public items: InventoryItem[];
 }
 
+export class ArtefactSpawner extends ObjectAtLocationHoc {
+  public spawner: string;
+}
+
+export class ArtefactSpawnerConfig {
+  public configs: ArtefactSpawnerType[];
+  public artefacts: Artefact[];
+}
+
+export class ArtefactSpawnerType {
+  public name: string;
+  public settings: ArtefactSpawnerSetting[];
+  public excludeRules: string[];
+  public listOfArtifacts: string[];
+  public useListOfArtifacts: boolean;
+}
+
+export class ArtefactSpawnerSetting {
+  public name: string;
+  public count: number;
+  public radius: number;
+  public minCooldown: number;
+  public maxCooldown: number;
+  public spawnChanceBase: number;
+  public spawnChanceBonus: number;
+  public rarityChance: number[];
+}
+
+export class Artefact {
+  public name: string;
+  public weight: number;
+  public rarity: string;
+  public detectorRequired: boolean;
+  public artifactType: string;
+  public archiartifactType: string;
+  public lifeTime: number;
+  public cost: number;
+}
